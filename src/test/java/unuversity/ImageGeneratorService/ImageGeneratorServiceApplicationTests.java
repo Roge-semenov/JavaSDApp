d464c8b4d315d71d3938ee103314da89664ec5ac
rogesemenov <egor27072004@gmail.com>
2024-05-13 23:46:14 +0300

    first commit

---
+++ src/test/java/unuversity/ImageGeneratorService/ImageGeneratorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package unuversity.ImageGeneratorService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageGeneratorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
